58f7af412e16cd25054bcaacc859f7f57380f123
khlam <khlam@users.noreply.github.com>
2018-09-04 17:14:54 -0700

    command line argument conversion

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import SiteHandler
 import packages.requests as requests
 import argparse
 
+import warnings
+warnings.filterwarnings("ignore")
+
 def confirmExit():
     input('\nPress the Enter key to exit')
     exit(0)
@@ -26,60 +29,41 @@ class AddonUpdater:
 
         try:
             self.WOW_ADDON_LOCATION = config['WOW ADDON UPDATER']['WoW Addon Location']
-            self.ADDON_LIST_FILE = config['WOW ADDON UPDATER']['Addon List File']
             self.INSTALLED_VERS_FILE = config['WOW ADDON UPDATER']['Installed Versions File']
-            self.AUTO_CLOSE = config['WOW ADDON UPDATER']['Close Automatically When Completed']
         except Exception:
-            print('Failed to parse configuration file. Are you sure it is formatted correctly?\n')
-            confirmExit()
-
-        if not isfile(self.ADDON_LIST_FILE):
-            print('Failed to read addon list file. Are you sure the file exists?\n')
-            confirmExit()
+            print('Failed to parse configuration file.')
 
         if not isfile(self.INSTALLED_VERS_FILE):
-            open(self.INSTALLED_VERS_FILE, 'w') # Create installed versions file since it doesn't exist
+            open(self.INSTALLED_VERS_FILE, 'w').close() # Create installed versions file since it doesn't exist
 
         return
 
-    def update(self):
-        uberlist = []
-        with open(self.ADDON_LIST_FILE, "r") as fin:
-            for line in fin:
-                current_node = []
-                line = line.rstrip('\n')
-                if not line or line.startswith('#'):
-                    continue
-                if '|' in line: # Expected input format: "mydomain.com/myzip.zip" or "mydomain.com/myzip.zip|subfolder"
-                    subfolder = line.split('|')[1]
-                    line = line.split('|')[0]
-                else:
-                    subfolder = ''
-                addonName = SiteHandler.getAddonName(line)
-                currentVersion = SiteHandler.getCurrentVersion(line)
-                if currentVersion is None:
-                    currentVersion = 'Not Available'
-                current_node.append(addonName)
-                current_node.append(currentVersion)
-                installedVersion = self.getInstalledVersion(line)
-                if not currentVersion == installedVersion:
-                    print('Installing/updating addon: ' + addonName + ' to version: ' + currentVersion + '\n')
-                    ziploc = SiteHandler.findZiploc(line)
-                    install_success = False
-                    install_success = self.getAddon(ziploc, subfolder) # install_success[1] is list of subfolders
-                    current_node.append(self.getInstalledVersion(line)) 
-                    if install_success[0] is True and currentVersion is not '':
-                        self.setInstalledVersion(line, currentVersion, install_success[1])
-                else:
-                    print(addonName + ' version ' + currentVersion + ' is up to date.\n')
-                    current_node.append("Up to date")
-                uberlist.append(current_node)
-        if self.AUTO_CLOSE == 'False':
-            col_width = max(len(word) for row in uberlist for word in row) + 2  # padding
-            print("".join(word.ljust(col_width) for word in ("Name","Iversion","Cversion")))
-            for row in uberlist:
-                print("".join(word.ljust(col_width) for word in row), end='\n')
-            confirmExit()
+    def install(self, addon_list):
+        for addon_url in addon_list:
+            addon_url = addon_url.rstrip('\n')
+
+            if not addon_url:
+                continue
+            if '|' in addon_url: # Expected input format: "mydomain.com/myzip.zip" or "mydomain.com/myzip.zip|subfolder"
+                subfolder = addon_url.split('|')[1]
+                addon_url = addon_url.split('|')[0]
+            else:
+                subfolder = ''
+
+            addonName = SiteHandler.getAddonName(addon_url)
+            currentVersion = SiteHandler.getCurrentVersion(addon_url)
+            if currentVersion is None:
+                currentVersion = 'Not Available'
+            installedVersion = self.getInstalledVersion(addon_url)
+            if not currentVersion == installedVersion:
+                print(addonName + '\t' + currentVersion + '\tInstalled/Updated')
+                ziploc = SiteHandler.findZiploc(addon_url)
+                install_success = False
+                install_success = self.getAddon(ziploc, subfolder) # install_success[1] is list of subfolders
+                if install_success[0] is True and currentVersion is not '':
+                    self.setInstalledVersion(addonName, addon_url, currentVersion, install_success[1])
+            else:
+                print(addonName + '\t' + currentVersion + '\tUp-to-date')
 
     def getAddon(self, ziploc, subfolder):
         if ziploc == '':
@@ -122,71 +106,66 @@ class AddonUpdater:
         except Exception:
             return 'version not found'
 
-    def setInstalledVersion(self, addonpage, currentVersion, folders):
-        addonName = addonpage
+    def setInstalledVersion(self, addonname, addonpage, currentVersion, folders):
         config = configparser.ConfigParser()
         config.read(self.INSTALLED_VERS_FILE)
         try:
-            config.get(addonName, 'version')    # If the addon is already installed, fetch
-            config.get(addonName, 'folders')    # its version and folders
+            config.get(addonpage, 'name')       # addon name
+            config.get(addonpage, 'version')    # addon version
+            config.get(addonpage, 'folders')    # addon subfolders
         except:
-            config.add_section(addonName)       # Otherwise create a new section
-        config.set(addonName, 'version', currentVersion)    # Set its version
-        config.set(addonName, 'folders', str(folders)[1 : -1].replace("'","").replace(" ",""))  # List all folders
-        with open(self.INSTALLED_VERS_FILE, 'w') as installedVersFile:                          # it creates
+            config.add_section(addonpage)                   # Otherwise create a new section
+        config.set(addonpage, 'name', addonname)            # Set addon name
+        config.set(addonpage, 'version', currentVersion)    # Set addon version
+        config.set(addonpage, 'folders', str(folders)[1 : -1].replace("'","").replace(" ",""))  # List all subfolders
+        with open(self.INSTALLED_VERS_FILE, 'w') as installedVersFile:
             config.write(installedVersFile)
 
-    def diff(self, a, b): # Returns all items in list a that are not in list b
-        return [item for item in a if item not in set(b)]
-
-    def uninstallAddon(self): # Allows user to uninstall addon by removing its URL from ADDON_LIST_FILE
-        with open(self.ADDON_LIST_FILE) as f:   # Read all URLs in ADDON_LIST_FILE into a list
-            content = [i.replace('#', '') for i in f.read().splitlines()]
-        config = configparser.ConfigParser()    # Read INSTALLED_VERS_FILE
+    def uninstall(self, addon_list):
+        config = configparser.ConfigParser()                        # Read INSTALLED_VERS_FILE
         config.read(self.INSTALLED_VERS_FILE)
-        installed = [section for section in config.sections()]  # Put all URLs in INSTALLED_VERS_FILE into a list
-        for to_del in self.diff(installed, content):            # Get diff of two lists. The resulting list
-            print("Uninstall {} ...".format(to_del))            # will be URLs not in the addon list file
+        for to_del in addon_list:
+            print("Uninstall {} ...".format(to_del))
             for folder in config.get(to_del, 'folders').split(','): # Proceed to delete all addon folders by reading
                 if (isdir(join(self.WOW_ADDON_LOCATION, folder))):  # the INSTALLED_VERS_FILE
-                    print("\t{}".format(folder))
+                    #print("\t{}".format(folder))
                     shutil.rmtree(join(self.WOW_ADDON_LOCATION, folder))
                 else:
                     print("\tCannot find {}".format(join(self.WOW_ADDON_LOCATION, folder)))
             config.remove_section(to_del)                           # Remove the addon entry from INSTALLED_VERS_FILE
             with open(self.INSTALLED_VERS_FILE, 'w') as installedVersFile:
                 config.write(installedVersFile)
-
+'''
 def main():
     addonupdater = AddonUpdater()
-    addonupdater.update()
+    addonupdater.install()
     addonupdater.uninstallAddon()
     return
-
+'''
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='World of Addons - An open source World of Warcraft addon manager')
 
     # python arg.py -l url1 url2 url3
-    parser.add_argument('-l','--list', nargs='+', required=True, help='<Required> Specifies addon URL list. ')
+    parser.add_argument('-l','--list', nargs='+', required=True, help='<Required> Specifies list of addon URLs.')
     
     # python arg.py -l url1 url2 url3 -i
-    parser.add_argument('-i', '--install', action='store_true', help='<Flag> If set script will update or install all specified addons.')
+    parser.add_argument('-i', '--install', action='store_true', help='<Flag> If set script will update or install all addons specified by -l.')
 
     # python arg.py -l url1 url2 url3 --uninstall
-    parser.add_argument('--uninstall', action='store_true', help='<Flag> If set script will uninstall all specified addons.')
+    parser.add_argument('--uninstall', action='store_true', help='<Flag> If set script will uninstall all addons specified by -l.')
 
     # python arg.py -l url1 url2 url3 -c
     parser.add_argument('-c', '--check', action='store_true', help='<Flag> If set script will check if update is available for specified addon.')
 
     args = parser.parse_args()
 
+    addonupdater = AddonUpdater()
+
     if (args.check == True):
         print("Checking the following addons for updates:\n\t{}".format(args.list))
 
     if (args.install == True and args.uninstall == False):
-        print("Updating/Installing the following addons:\n\t{}".format(args.list))
+        addonupdater.install(args.list)
 
     if (args.install == False and args.uninstall == True):
-        print("Uninstalling the following addons:\n\t{}".format(args.list))
-
-#    main()
+        addonupdater.uninstall(args.list)
